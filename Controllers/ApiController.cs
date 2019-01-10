dcd9c32f8105abc22432236302ea7889cc8b9c98
ncrone001 <ncrone001@NCRONE00113>
2019-01-10 17:22:57 -0600

    adding to repo

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReactAdminV2.Controllers
+{
+    public class ApiController : ControllerBase
+    {
+        private IEnumerable<object> _authors = new[] { new {
+            Id = 0,
+            FirstName = "",
+            LastName = ""
+        }};
+
+        [HttpGet]
+        public ActionResult<IEnumerable<object>> GetAllAuthors()
+        {
+            return _authors.ToList();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<object>> GetAuthorById(int Id)
+        {
+
+            return _authors.ToList();
+        }
+    }
+}
